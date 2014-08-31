9a35db9b540309889cd7718ee133445f087ac5e3
Hudson Marinho <hudsonmarinhodev@gmail.com>
2014-08-31 14:47:00 -0300

    changes Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,53 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-  });
+"use strict";
 
-  // grunt.loadNpmTasks('grunt-lintspaces');
+module.exports = function(grunt)
+{
+	// Load all tasks
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		// Watch
+		watch: {
+			css: {
+				files: ['_sources/stylesheets/**/*' ],
+				tasks: ['compass']
+			},
+			js: {
+				files: '_sources/javascripts/**/*',
+				tasks: ['uglify']
+			}
+		},
+
+		// Compile scss
+		compass: {
+			dist: {
+				options: {
+					force: true,
+					config: 'config.rb',
+					outputStyle: 'compressed'
+				}
+			}
+		},
+
+		// Concat and minify javascripts
+		uglify: {
+			options: {
+				mangle: false
+			},
+			dist: {
+				files: {
+					'public/javascripts/app.js': [
+						'_sources/javascripts/libs/modernizr-2.6.2.min.js',
+						'_sources/javascripts/libs/jquery-1.10.2.min.js',
+            
+						'_sources/javascripts/script.js',
+					]
+				}
+			}
+		},
+	});
+
+	// registrando tarefa default
+	grunt.registerTask('default',	['watch']);
+
 };
