4859094e8b7ab3d053320abc0e0f43443aa3f45c
Hudson Marinho <hudsonmarinhodev@gmail.com>
2015-08-06 20:43:54 -0300

    add initial assets

---
+++ source/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require "vendors/trianglify.min"
+//= require "vendors/TweenMax.min"
+//= require "vendors/ScrollToPlugin.min"
+//= require "vendors/cash.min"
+
+//= require "Card-polygon-4"
+//= require "demo-4"
